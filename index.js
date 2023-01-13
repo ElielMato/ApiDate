67c94d860a54130262d31f4adf2d7057011e8da6
ElielMato <86053896+ElielMato@users.noreply.github.com>
2023-01-12 21:23:13 -0300

    ApiDate

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+let dateNow = new Date()
+let year = dateNow.getFullYear()
+let day = dateNow.getDate()
+let monthYear = ['Enero', 'Febrero', 'Marzo', 'Abri', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+
+app.get('/what-date', function (req, res) {
+    res.send(`Fecha Actual: ${day} de ${monthYear[dateNow.getMonth()]} del ${year}`)
+})
+
+app.get('/what-time', function (req, res) {
+    res.send(`Horario de Hoy: ${dateNow.toLocaleTimeString()}`)
+})
+
+app.get('/what-date-time', function (req, res) {
+    res.send(`Fecha y Horario Actual: ${day} de ${monthYear[dateNow.getMonth()]} del ${year} - ${dateNow.toLocaleTimeString()}`)
+})
+
+app.listen(3000)
+
